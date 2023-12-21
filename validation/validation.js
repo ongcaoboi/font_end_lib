d1b20914575935f02f7056410dd50d6a71464962
Tuấn Anh <tuananhafiy@gmail.com>
2023-12-21 21:28:04 +0700

    add validate select-option

--- validation/validation.js
+++ validation/validation.js
@@ -1,40 +1,39 @@
 function Validation(options) {
   let defaultOptions = {
-    errorSelector: '.error-message',
-    classError: 'invalid',
-    formGroupSelector: '.form-group'
+    errorSelector: '.fg-error-mg-validate',
+    classError: 'fg-invalid-validate',
+    formGroupSelector: '.fg-validate'
   }
 
   function getParentElement(element, selector) {
     while (element.parentElement) {
       if (element.parentElement.matches(selector)) {
-        return element.parentElement
+        return element.parentElement;
       }
-      element = element.parentElement
+      element = element.parentElement;
     }
   }
 
   function getErrorElement(element) {
-    let formGroupElement = getParentElement(element, options.formGroupSelector)
-    return formGroupElement.querySelector(options.errorSelector)
+    let formGroupElement = getParentElement(element, options.formGroupSelector);
+    return formGroupElement.querySelector(options.errorSelector);
   }
 
-  options = { ...defaultOptions, ...options }
-  let formValues = {}
+  options = { ...defaultOptions, ...options };
 
-  const formElement = document.querySelector(options.form)
-  const queryOne = formElement.querySelector.bind(formElement)
-  const queryAll = formElement.querySelectorAll.bind(formElement)
+  const formElement = document.querySelector(options.form);
+  const queryOne = formElement.querySelector.bind(formElement);
+  const queryAll = formElement.querySelectorAll.bind(formElement);
 
-  let selectorRules = {}
+  let selectorRules = {};
 
   function getValueFromControl(control) {
     if (control.type === 'checkbox') {
-      const array = []
-      const elements = queryAll(control.selector)
+      const array = [];
+      const elements = queryAll(control.selector);
       for (let i = 0; i < elements.length; i++) {
         if (elements[i].checked) {
-          array.push(elements[i].value)
+          array.push(elements[i].value);
         }
       }
       return {
@@ -43,11 +42,11 @@ function Validation(options) {
       }
 
     } else if (control.type === 'radio') {
-      const elements = queryAll(control.selector)
+      const elements = queryAll(control.selector);
       let value = null;
       for (let i = 0; i < elements.length; i++) {
         if (elements[i].checked) {
-          value = elements[i].value
+          value = elements[i].value;
         }
       }
       return {
@@ -55,15 +54,23 @@ function Validation(options) {
         value: value
       }
     } else if (control.type === 'file') {
-      const element = queryOne(control.selector)
+      const element = queryOne(control.selector);
       const value = element.files.length > 0 ? element.files : null;
       return {
         name: element.name,
         value: value
       }
+    } else if (control.type === 'select') {
+      const element = queryOne(control.selector);
+      console.log({element}, element.value);
+      const value = element.value;
+      return {
+        name: element.name,
+        value: value
+      }
     } else {
-      const element = queryOne(control.selector)
-      const value = element.value
+      const element = queryOne(control.selector);
+      const value = element.value;
       return {
         name: element.name,
         value: element.value
@@ -73,7 +80,7 @@ function Validation(options) {
 
   function regexValidator(value, regex, message) {
     if (typeof message === 'undefined') {
-      message = 'Invalid format'
+      message = 'Invalid format';
     }
     return regex.test(value) ? '' : message;
   }
@@ -81,7 +88,7 @@ function Validation(options) {
   function addEventBlur(inputElement, rule) {
     if (inputElement) {
       inputElement.onblur = () => {
-        validate(inputElement, rule)
+        validate(inputElement, rule);
       }
     }
   }
@@ -89,10 +96,10 @@ function Validation(options) {
   function addEventInput(inputElement) {
     if (inputElement) {
       inputElement.oninput = () => {
-        let formGroupElement = getParentElement(inputElement, options.formGroupSelector)
-        let errorElement = formGroupElement.querySelector(options.errorSelector)
-        errorElement.innerText = ''
-        formGroupElement.classList.remove(options.classError)
+        let formGroupElement = getParentElement(inputElement, options.formGroupSelector);
+        let errorElement = formGroupElement.querySelector(options.errorSelector);
+        errorElement.innerText = '';
+        formGroupElement.classList.remove(options.classError);
       }
     }
   }
@@ -106,31 +113,42 @@ function Validation(options) {
   }
 
   let validate = (inputElement, elem) => {
-    let formGroupElement = getParentElement(inputElement, options.formGroupSelector)
-    let errorElement = formGroupElement.querySelector(options.errorSelector)
-    let errorMessage = ''
-    let rules = selectorRules[elem.selector]
-    let valueElm = getValueFromControl(elem).value
+    let formGroupElement = getParentElement(inputElement, options.formGroupSelector);
+    let errorElement = formGroupElement.querySelector(options.errorSelector);
+    let errorMessage = '';
+    let rules = selectorRules[elem.selector];
+    let valueElm = getValueFromControl(elem).value;
 
     for (let i = 0; i < rules.length; i++) {
-      const rule = rules[i]
+      const rule = rules[i];
 
       if (rule.regex instanceof RegExp) {
-        errorMessage += regexValidator(valueElm, rule.regex, rule.message)
+        errorMessage += regexValidator(valueElm, rule.regex, rule.message);
       }
 
       if (errorMessage.length === 0 && typeof rule.test === 'function') {
-        errorMessage += rule.test(valueElm)
+        const result = rule.test(valueElm)
+        if (typeof result === 'string') {
+          errorMessage += result;
+        } else {
+          if (result === false) {
+            errorMessage += rule.message;
+          }
+        }
       }
     }
 
     if (errorMessage) {
-      errorElement.innerText = errorMessage
-      formGroupElement.classList.add(options.classError)
+      if (errorElement) {
+        errorElement.innerText = errorMessage;
+      }
+      formGroupElement.classList.add(options.classError);
       return false
     } else {
-      errorElement.innerText = ''
-      formGroupElement.classList.remove(options.classError)
+      if (errorElement) {
+        errorElement.innerText = '';
+      }
+      formGroupElement.classList.remove(options.classError);
       return true
     }
   }
@@ -140,18 +158,18 @@ function Validation(options) {
 
       ruleTest = {}
       if (rule.regex) {
-        ruleTest.regex = rule.regex
+        ruleTest.regex = rule.regex;
       }
       if (rule.test) {
-        ruleTest.test = rule.test
+        ruleTest.test = rule.test;
       }
       if (rule.message) {
-        ruleTest.message = rule.message
+        ruleTest.message = rule.message;
       }
       if (Array.isArray(selectorRules[rule.selector])) {
-        selectorRules[rule.selector].push(ruleTest)
+        selectorRules[rule.selector].push(ruleTest);
       } else {
-        selectorRules[rule.selector] = [ruleTest]
+        selectorRules[rule.selector] = [ruleTest];
       }
 
       switch (rule.type) {
@@ -159,39 +177,44 @@ function Validation(options) {
         case 'checkbox': {
           const elements = queryAll(rule.selector)
           for (let i = 0; i < elements.length; i++) {
-            addEventInput(elements[i])
-            addEventChange(elements[i], rule)
+            addEventInput(elements[i]);
+            addEventChange(elements[i], rule);
           }
           break
         }
         case 'file': {
           let inputElement = queryOne(rule.selector)
-          addEventInput(inputElement)
-          addEventChange(inputElement, rule)
+          addEventInput(inputElement);
+          addEventChange(inputElement, rule);
 
           break
         }
+        case 'select': {
+          let selectElement = queryOne(rule.selector);
+          addEventChange(selectElement, rule);
+          break;
+        }
         default: {
-          let inputElement = queryOne(rule.selector)
-          addEventBlur(inputElement, rule)
-          addEventInput(inputElement)
+          let inputElement = queryOne(rule.selector);
+          addEventBlur(inputElement, rule);
+          addEventInput(inputElement);
         }
       }
     })
 
     formElement.onsubmit = (e) => {
-      e.preventDefault()
+      e.preventDefault();
 
       let firstInvalidInput = null;
 
-      let isFormValid = true
+      let isFormValid = true;
 
       options.rules.forEach((rule) => {
-        let isValid = false
+        let isValid = false;
         switch (rule.type) {
           case 'radio':
           case 'checkbox': {
-            const elements = queryAll(rule.selector)
+            const elements = queryAll(rule.selector);
             for (let i = 0; i < elements.length; i++) {
               isValid = validate(elements[i], rule);
 
@@ -202,7 +225,7 @@ function Validation(options) {
             break
           }
           case 'file': {
-            let inputElement = queryOne(rule.selector)
+            let inputElement = queryOne(rule.selector);
             isValid = validate(inputElement, rule);
 
             if (!isValid && !firstInvalidInput) {
@@ -211,7 +234,7 @@ function Validation(options) {
             break
           }
           default: {
-            let inputElement = queryOne(rule.selector)
+            let inputElement = queryOne(rule.selector);
             isValid = validate(inputElement, rule);
 
             if (!isValid && !firstInvalidInput) {
@@ -220,7 +243,7 @@ function Validation(options) {
           }
         }
         if (!isValid) {
-          isFormValid = false
+          isFormValid = false;
         }
       })
 
@@ -230,14 +253,14 @@ function Validation(options) {
 
       if (isFormValid) {
         if (typeof (options.onSubmit) === 'function') {
-          const formValues = {}
+          const formValues = {};
           options.rules.forEach((rule) => {
-            const { name, value } = getValueFromControl(rule)
-            formValues[name] = value
+            const { name, value } = getValueFromControl(rule);
+            formValues[name] = value;
           })
-          return options.onSubmit(formValues)
+          return options.onSubmit(formValues);
         } else {
-          formElement.submit()
+          formElement.submit();
         }
       }
     }
